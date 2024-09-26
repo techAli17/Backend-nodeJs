06f164e8848cd8207b86e2e6e7fedc904d338935
kayyoomAli <kayyoom.ali@gosamplify.com>
2024-09-26 22:16:36 +0530

    utils codes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // require('dotenv').config() this also works but we need to use dotenv as a package module as import
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
